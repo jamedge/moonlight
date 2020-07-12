440536bafac3f46a45b8d3c082d5ee9de87b31ee
Marko Jamedzija <marko.jamedzija@hotmail.com>
2020-07-12 22:07:58 +0200

    add reading of the remaining line properties

--- moonlight-core/src/main/scala/com/github/jamedge/moonlight/core/api/versioning/line/LineMDGenerator.scala
+++ moonlight-core/src/main/scala/com/github/jamedge/moonlight/core/api/versioning/line/LineMDGenerator.scala
@@ -1,6 +1,6 @@
 package com.github.jamedge.moonlight.core.api.versioning.line
 
-import com.github.jamedge.moonlight.core.model.{IOElement, Line}
+import com.github.jamedge.moonlight.core.model.{Alert, Code, IOElement, Line, Metric, Process}
 
 import scala.util.Try
 
@@ -11,12 +11,17 @@ class LineMDGenerator {
       val result =
         s"""Property name|Property value
            |-------------|--------------
-           |name|${line.name}
-           |owner|${line.owner.getOrElse(naString)}
-           |purpose|${line.purpose.getOrElse(naString)}
-           |notes|${line.notes.getOrElse(List()).map(n => s"_${n}_").mkString(", ")}
-           |inputs|${line.io.flatMap(io => generateIOCaptions(io.inputs)).mkString(", ")}
-           |outputs|${line.io.flatMap(io => generateIOCaptions(io.outputs)).mkString(", ")}
+           |Name|**${line.name}**
+           |Owner|${line.owner.getOrElse(naString)}
+           |Purpose|${line.purpose.getOrElse(naString)}
+           |Notes|${line.notes.getOrElse(List()).map(n => s"_${n}_").mkString(", ")}
+           |Inputs|${line.io.flatMap(io => generateIOCaptions(io.inputs)).mkString(", ")}
+           |Outputs|${line.io.flatMap(io => generateIOCaptions(io.outputs)).mkString(", ")}
+           |Processed by|${generateProcessCaptions(line.processedBy).mkString(", ")}
+           |Metrics|${generateMetricsCaptions(line.metrics).mkString(", ")}
+           |Alerts|${generateAlertsCaptions(line.alerts).mkString(", ")}
+           |Code path|${line.code.map(c => s"[${c.name}](${c.remotePath})").getOrElse(naString)}
+           |Execution command entry point|${line.code.map(c => generateExecutionCommand(c)).getOrElse(naString)}
           """.stripMargin
       result
     }
@@ -26,9 +31,43 @@ class LineMDGenerator {
     ioElements.map { i =>
       i.storage.map { s =>
         s"[**${s.name}**: ${i.name}]" +
-          s"${s.locationPath.map(slp => s"($slp${if (slp.endsWith("/")) "" else "/"}").getOrElse("(#")}" +
-          s"${i.locationRelativePath.map(ilp => s"$ilp)").getOrElse(s"${i.name})")}"
-      }.getOrElse("")
+          s"${s.locationPath.map(lp => s"($lp${if (lp.endsWith("/")) "" else "/"}").getOrElse("(#")}" +
+          s"${i.locationRelativePath.map(rp => s"$rp)").getOrElse(s"${i.name})")}"
+      }.getOrElse(s"[${i.name}](#${i.locationRelativePath.getOrElse(i.name)})")
     }
+  }
+
+  private def generateProcessCaptions(processes: List[Process]): List[String] = {
+    processes.map { p =>
+      p.processingFramework.map { pf =>
+        s"[**${pf.name}**: ${p.name}]" +
+          s"${pf.locationPath.map(fp => s"($fp${if (fp.endsWith("/")) "" else "/"}").getOrElse("(#")}" +
+          s"${p.locationRelativePath.map(rp => s"$rp)").getOrElse(s"${p.name})")}"
+      }.getOrElse(s"[${p.name}](#${p.locationRelativePath.getOrElse(p.name)})")
+    }
+  }
+
+  private def generateMetricsCaptions(metrics: List[Metric]): List[String] = {
+    metrics.map { m =>
+      m.metricFramework.map { mf =>
+        s"[**${mf.name}**: ${m.name}]" +
+          s"${mf.locationPath.map(fp => s"($fp${if (fp.endsWith("/")) "" else "/"}").getOrElse("(#")}" +
+          s"${m.locationRelativePath.map(rp => s"$rp)").getOrElse(s"${m.name})")}"
+      }.getOrElse(s"[${m.name}](#${m.locationRelativePath.getOrElse(m.name)})")
+    }
+  }
+
+  private def generateAlertsCaptions(alerts: List[Alert]): List[String] = {
+    alerts.map { a =>
+      a.alertsFramework.map { af =>
+        s"[**${af.name}**: ${a.name}]" +
+          s"${af.locationPath.map(fp => s"($fp${if (fp.endsWith("/")) "" else "/"}").getOrElse("(#")}" +
+          s"${a.locationRelativePath.map(rp => s"$rp)").getOrElse(s"${a.name})")}"
+      }.getOrElse(s"[${a.name}](#${a.locationRelativePath.getOrElse(a.name)})")
+    }
+  }
+
+  private def generateExecutionCommand(code: Code): String = {
+    s"`${code.entryPointClass}${code.entryPointArguments.map(epa => s" ${epa.mkString(" ")}`").getOrElse("`")}"
   }
 }
