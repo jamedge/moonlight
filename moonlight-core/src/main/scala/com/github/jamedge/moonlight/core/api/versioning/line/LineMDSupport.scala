ff7e5019b3b7bd53840363fcb29da8cdd20b1ad0
Marko Jamedzija <marko.jamedzija@hotmail.com>
2020-07-15 21:13:26 +0200

    I11 add line md marshaller (#14)
    * add line markdown marshaller
    
    * update the docs

---
+++ moonlight-core/src/main/scala/com/github/jamedge/moonlight/core/api/versioning/line/LineMDSupport.scala
@@ -0,0 +1,44 @@
+package com.github.jamedge.moonlight.core.api.versioning.line
+
+import akka.http.scaladsl.marshalling.{Marshaller, ToResponseMarshaller}
+import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse, MediaTypes}
+import com.github.jamedge.moonlight.core.api.versioning.{HTMLSupport, MDSupport, MediaVersionTypes}
+import com.github.jamedge.moonlight.core.model.Line
+
+import scala.concurrent.ExecutionContext
+
+object LineMDSupport extends MDSupport[Line] {
+  override implicit def marshaller(
+      implicit executionContext: ExecutionContext,
+      lineMDGenerator: LineMDGenerator
+  ): ToResponseMarshaller[Line] = {
+    Marshaller.oneOf(
+      mdEntityMarshaller,
+      mdV1EntityMarshaller
+    )
+  }
+
+  private def mdEntityMarshaller(
+      implicit executionContext: ExecutionContext,
+      lineMDGenerator: LineMDGenerator
+  ): ToResponseMarshaller[Line] = {
+    Marshaller.withOpenCharset(MediaTypes.`text/markdown`) { case (line, charset) =>
+      HttpResponse(entity = HttpEntity(
+        ContentType(MediaTypes.`text/markdown`, charset),
+        lineMDGenerator.generateMd(line).getOrElse(throw new LineMDGenerationException("Error generating line MD!"))))
+    }
+  }
+
+  private def mdV1EntityMarshaller(
+      implicit executionContext: ExecutionContext,
+      lineMDGenerator: LineMDGenerator
+  ): ToResponseMarshaller[Line] = {
+    Marshaller.withFixedContentType(MediaVersionTypes.`text/moonlight.v1+markdown`) { line =>
+      HttpResponse(entity = HttpEntity(
+        ContentType(MediaVersionTypes.`text/moonlight.v1+markdown`),
+        lineMDGenerator.generateMd(line).getOrElse(throw new LineMDGenerationException("Error generating line MD!"))))
+    }
+  }
+}
+
+case class LineMDGenerationException(message: String) extends Exception(message)
