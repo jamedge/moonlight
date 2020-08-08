fd051e211ed785333712ec0244d0b7c07c606958
Marko Jamedzija <marko.jamedzija@hotmail.com>
2020-08-09 00:03:05 +0200

    minor import exclusion

--- moonlight-core/src/main/scala/com/github/jamedge/moonlight/core/Context.scala
+++ moonlight-core/src/main/scala/com/github/jamedge/moonlight/core/Context.scala
@@ -3,7 +3,6 @@ package com.github.jamedge.moonlight.core
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 
 class Context(app: String) {
